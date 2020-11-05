726310884163161a17674973ce88a50ae494d890
Juan Arango <juan.arango@olsoftware.com>
2020-11-05 14:10:03 -0500

    Carga de la barra de energía al pegarle a un enemigo

---
+++ Assets/Scripts/EnergyBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnergyBar : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+
+    public void SetMaxEnergy(int energy, int initialEnergy)
+    {
+        slider.maxValue = energy;
+        slider.value = initialEnergy;
+
+        fill.color = gradient.Evaluate(1f);
+    }
+
+
+    public void SetEnergy(int energy)
+    {
+        slider.value = energy;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+}
