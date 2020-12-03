e5b5bec93d8b21c9b835be2c938096ab9b2bba3e
jdvrossi <jdvrossi@uao.edu.co>
2020-12-03 11:27:39 -0500

    Sonidos del robot
    act

--- Assets/Scripts/Enemies/EnemySpecific/Enemy1/E1_DeadState.cs
+++ Assets/Scripts/Enemies/EnemySpecific/Enemy1/E1_DeadState.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class E1_DeadState : DeadState
 {
+
     private Enemy1 enemy;
     public E1_DeadState(Entity etity, FiniteStateMachine stateMachine, string animBoolName, D_DeadState stateData, Enemy1 enemy) : base(etity, stateMachine, animBoolName, stateData)
     {
